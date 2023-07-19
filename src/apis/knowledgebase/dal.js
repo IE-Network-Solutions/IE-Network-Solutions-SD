0ce602143088c9cc379cf9904f7c949d7d84f74c
adgehTech@gmail.com <belaybirhanu@gmail.com>
2023-07-19 16:00:28 +0300

    crud api for knowledgebase added

---
+++ src/apis/knowledgebase/dal.js
@@ -0,0 +1,89 @@
+const { getConnection } = require("typeorm");
+const KnowledgeBase = require("../../models/KnowledgeBase"); 
+
+class KnowledgeBaseDAL {
+  static async getAllKnowledgebase() {
+    try {
+      const connection = await getConnection();
+
+      const knowledgebaseRepository = connection.getRepository(KnowledgeBase);
+
+      const knowledgebases = await knowledgebaseRepository.find();
+      // return all fetched data.
+      return knowledgebases;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async getKnowlegebaseById(id) {
+    try {
+      const connection = await getConnection();
+
+      const knowledgebaseRepository = connection.getRepository(KnowledgeBase);
+
+      const knowledgebase = await knowledgebaseRepository.findOneBy({
+        id: id,
+      });
+      return knowledgebase;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async createKnowledgebase(data) {
+    try {
+      const { title, category, description } = data;
+
+      const connection = getConnection();
+
+      const knowledgebaseRepository = connection.getRepository(KnowledgeBase);
+
+      const newKnowledgebase = await knowledgebaseRepository.create({
+        title,
+        category,
+        description,
+      });
+
+      await knowledgebaseRepository.save(newKnowledgebase);
+
+      return newKnowledgebase;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async updateOneKnowledgebase(id, updatedFields) {
+    const connection = getConnection();
+
+    const knowledgebaseRepository = connection.getRepository(KnowledgeBase);
+
+    const knowledgebase = await knowledgebaseRepository.findOneBy({ id });
+
+    if (!knowledgebase) {
+      throw new Error("Knowledgebase with the given id is not found");
+    }
+    
+    // referesh the updated at field.
+    updatedFields.updated_at = new Date();
+
+    knowledgebaseRepository.merge(knowledgebase, updatedFields);
+
+    await knowledgebaseRepository.save(knowledgebase);
+
+    // return updated knowlegebase data.
+    return knowledgebase;
+  }
+
+  static async deleteOneKnowledgebase(id) {
+    const connection = getConnection();
+
+    const knowledgebaseRepository = connection.getRepository(KnowledgeBase);
+
+    await knowledgebaseRepository.delete(id);
+
+    return "Test deleted Successfully";
+  }
+}
+
+module.exports = KnowledgeBaseDAL;
---
+++ src/apis/knowledgebase/router.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const knowledgebaseController = require("./controller");
+const createKnowledgebaseValidator = require("./validation");
+const validate = require("../../../utils/validator");
+ 
+router.route("/").get(knowledgebaseController.getAllKnowledgebase);
+router.route("/").post(validate(createKnowledgebaseValidator), knowledgebaseController.createKnowledgebase);
+router.route("/:id").get(knowledgebaseController.getKnowlegebaseById)
+router.route("/:id").patch(knowledgebaseController.updateOneKnowledgebase)
+router.route("/:id").delete(knowledgebaseController.deleteOneKnowledgebase);
+
+module.exports = router;

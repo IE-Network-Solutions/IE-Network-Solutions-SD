1064be0ee8f322c8b1ff47c21329a73c5a9b6a67
dagmawibabi <1babidagi@gmail.com>
2023-07-19 14:40:32 +0300

    started jwt

--- src/apis/users/router.js
+++ src/apis/users/router.js
@@ -6,11 +6,12 @@ router.route("/getAllUsers").get(UserController.getAllUsers);
 router.route("/getOneUser/:id").get(UserController.getOneUser);
 
 router.route("/createUser").post(UserController.createUser);
+router.route("/editUser").post(UserController.editUser);
 
 router.route("/deleteUser").delete(UserController.deleteUser);
 router.route("/deleteAllUsers").delete(UserController.deleteAllUsers);
 
-router.route("/editUser").post(UserController.editUser);
+
 router.route("/loginUser").post(UserController.loginUser);
 router.route("/logoutUser").post(UserController.logoutUser);
 

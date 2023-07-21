1c01eb0b33f2a0ae8f4e43cede34eac6920ed4e3
hiluf.meressa@ienetworksolutions.com <hmkahsay@gmail.com>
2023-07-21 09:17:28 +0300

    catagory model is added

---
+++ src/models/Catagoreis.js
@@ -0,0 +1,41 @@
+// const { EntitySchema } = require("typeorm"); 
+
+// const Catagories = new EntitySchema({
+//   name: "Catagory",
+//   columns: {
+//     id: {
+//       primary: true,
+//       type: "uuid",
+//       generated: "uuid",
+//     },
+//     title: {
+//       type: "varchar",
+//     },
+//     category: {
+//       type: "varchar",
+//     },
+//     description: {
+//       type: "text",
+//     },
+//     created_at: {
+//       type: "timestamp",
+//       default: () => "CURRENT_TIMESTAMP",
+//     },
+//     updated_at: {
+//       type: "timestamp",
+//       default: () => "CURRENT_TIMESTAMP",
+//     },
+//   },
+//   relations: {
+//     created_by: {
+//       type: "many-to-one",
+//       target: "User",
+//       joinColumn: {
+//         name: "user_id",
+//         referencedColumnName: "id",
+//       },
+//     },
+//   },
+// });
+
+// module.exports = KnowledgeBase;
